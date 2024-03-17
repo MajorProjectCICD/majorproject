64cf8325f82ea0d4a61011b4f0dc471d95d244ee
Tanish2002 <tanishkhare@gmail.com>
2024-03-18 01:00:00 +0530

    fix: e2e test workflow

--- cypress/e2e/app.cy.ts
+++ cypress/e2e/app.cy.ts
@@ -1,13 +1,13 @@
 describe('Navigation', () => {
   it('should navigate to the Register Page', () => {
     cy.visit('http://localhost:3000/');
-    cy.get('a[href*="/auth/register"]').click({ multiple: true });
+    cy.get('a[href*="/auth/register"]').first().click();
     cy.url().should('include', '/auth/register');
     cy.get('h2').contains('Register Student');
   });
   it('should navigate to the login Page', () => {
     cy.visit('http://localhost:3000/');
-    cy.get('a[href*="/auth/login"]').click({ multiple: true });
+    cy.get('a[href*="/auth/login"]').first().click();
     cy.url().should('include', '/auth/login');
     cy.get('h2').contains('Login Student');
   });
